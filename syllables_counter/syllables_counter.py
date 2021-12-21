804137d84542b191397bbf803a6dade9acc1e375
Aleksey Petukhov <ikazman@yandex.ru>
2021-12-21 16:10:30 +0300

    Simple syllables counter ready

--- syllables_counter/syllables_counter.py
+++ syllables_counter/syllables_counter.py
@@ -1,1 +1,27 @@
-.
+VOWELS = 'АЕЁИОУЫЭЮЯаеёиоуыэюя'
+
+def syllables_counter(phrase):
+    """Считаем число слогов в слове."""
+    num_of_syllas = 0
+    num_of_vowels = 0
+    phrase = phrase.casefold().strip().split()
+
+    for word in phrase:
+        for char in word:
+            if char in VOWELS:
+                num_of_syllas += 1
+                num_of_vowels += 1
+        if num_of_vowels == 0:
+            num_of_syllas += 1
+
+    return num_of_syllas     
+
+def main():
+    phrase_one = 'Я - Гайбраш Трипвуд, могущественный пират!'
+    phrase_two = 'Арррррргх!'
+    print('Считаем. Число слогов:')
+    print(f'- во фразе "{phrase_one}" - {syllables_counter(phrase_one)}')
+    print(f'- во фразе "{phrase_two}" - {syllables_counter(phrase_two)}')
+
+if __name__ == '__main__':
+    main()
