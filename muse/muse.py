dac5a34231a79752ff9a287fb2251df8c830d627
Aleksey Petukhov <ikazman@yandex.ru>
2021-12-21 20:44:51 +0300

    Muse ready

--- muse/muse.py
+++ muse/muse.py
@@ -1,17 +1,53 @@
-import random
 from collections import defaultdict
+
+from syllables_counter import syllables_counter
 
 class Muse:
     """Цепь Маркова."""
 
     def __init__(self, corpus):
-        pass
+        self.corpus = corpus
+        self.length = len(corpus)
+        self.dict_word_to_word = defaultdict(list)
+        self.dict_word_to_two_word = defaultdict(list)
 
     def map_word_to_word(self):
-        pass
+        """Размечиваем второе слово после первого."""
+        limit = self.length - 1
+        for idx, word in enumerate(self.corpus):
+            if idx < limit:
+                suffix = self.corpus[idx + 1]
+                self.dict_word_to_word[word].append(suffix)
 
-    def map_two_words_to_word(self):
-        pass
+    def map_word_to_two_words(self):
+        """Размечиваем третье слово после первых двух."""
+        limit = self.length - 2
+        for idx, word in enumerate(self.corpus):
+            if idx < limit:
+                key = ' '.join(word, self.corpus[idx + 1])
+                suffix = self.corpus[idx + 2]
+                self.dict_word_to_two_word[key].append(suffix)
 
-    def pick_the_word(self):
-        pass
+    def word_after_single(self, prefix, suffix_map, current_syls, target_syls):
+        """Возвращаем все допустимые варианты после первого слова из копуса."""
+        accepted_words = []
+        suffixes = suffix_map.get(prefix)
+        if suffixes:
+            for candidate in suffixes:
+                num_syls = syllables_counter.syllables_counter(candidate)
+                if current_syls + num_syls < target_syls:
+                    accepted_words.append(candidate)
+        return accepted_words
+
+    def word_after_double(self, prefix, suffix_map, current_syls, target_syls):
+        """Возвращаем все допустимые варианты после пары слов из копуса."""
+        accepted_words = []
+        suffixes = suffix_map.get(prefix)
+        if suffixes:
+            for candidate in suffixes:
+                num_syls = syllables_counter.syllables_counter(candidate)
+                if current_syls + num_syls < target_syls:
+                    accepted_words.append(candidate)
+        return accepted_words
+
+
--- poet/compose.py
+++ poet/compose.py
@@ -1,1 +1,12 @@
-.
+import random
+
+from syllables_counter import syllables_counter
+    
+    def pick_the_word(self):
+        """Выбираем случайное слово и считаем число слогов."""
+        first_word = random.choise(self.corpus)
+        num_syls = syllables_counter.syllables_counter(first_word)
+        if num_syls > 4:
+            self.pick_the_word(self.corpus)
+        else:
+            return(first_word, num_syls)
