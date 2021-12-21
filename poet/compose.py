e830767138e39fe35fd5663b6e1e2c14ad9a9a60
Aleksey Petukhov <ikazman@yandex.ru>
2021-12-21 18:13:43 +0300

    Added muse

--- poet/compose.py
+++ poet/compose.py
@@ -1,17 +1,1 @@
-import random
-from collections import defaultdict
-
-class Muse:
-    """Цепь Маркова."""
-
-    def __init__(self):
-        pass
-
-    def map_word_to_word(self):
-        pass
-
-    def map_two_words_to_word(self):
-        pass
-
-    def pick_the_word(self):
-        pass
+.
